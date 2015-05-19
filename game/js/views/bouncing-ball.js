f19457cc966b4b6253595824685f17429dcc033f
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-05-19 20:11:26 +1000

    now with background

--- game/js/views/bouncing-ball.js
+++ game/js/views/bouncing-ball.js
@@ -28,15 +28,49 @@ module.exports = {
       return state['bouncing-ball-game'].ball.demeanour;
     };
 
+    var theBallRadius = function (state) {
+      return state['bouncing-ball-game'].ball.radius;
+    };
+
+    var theBoardDimensions = function (state) {
+      return state['bouncing-ball-game'].board;
+    };
+
+    var calculateOffset = function (boardDimensions, screenDimensions) {
+      return {
+        x: (screenDimensions.usableWidth - boardDimensions.width) / 2,
+        y: (screenDimensions.usableHeight - boardDimensions.height) / 2
+      };
+    };
+
+    var createBall = function () {
+      var ball = new PIXI.Graphics();
+      ball.beginFill(0xffffff);
+      ball.drawCircle(0, 0, tracker().get(theBallRadius));
+
+      return ball;
+    };
+
+    var createBoard = function () {
+      var board = new PIXI.Graphics();
+      board.beginFill(0x55ff55);
+      board.drawRect(0, 0, tracker().get(theBoardDimensions).width, tracker().get(theBoardDimensions).height);
+
+      return board;
+    };
+
+    var offset;
     return function (dims) {
       var stage = new PIXI.Container();
       var renderer = PIXI.autoDetectRenderer(dims.usableWidth, dims.usableHeight);
       $('#' + element()).append(renderer.view);
 
-      var ball = new PIXI.Graphics();
-      ball.beginFill(0xffffff);
-      ball.drawCircle(0,0,25);
+      offset = calculateOffset(tracker().get(theBoardDimensions), dims);
+      stage.position.x = offset.x;
+      stage.position.y = offset.y;
 
+      var ball = createBall();
+      stage.addChild(createBoard());
       stage.addChild(ball);
 
       tracker().onChangeOf(theBallPosition, updateBall, ball);
